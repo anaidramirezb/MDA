dd1f65ab2913bcca82cc6c6c1a845d3e22162ee8
heckutoru <heckutoru@gmail.com>
2019-03-24 00:45:52 +0000

    v10

--- src/main/java/com/mycompany/mda/dbHandler.java
+++ src/main/java/com/mycompany/mda/dbHandler.java
@@ -1,9 +1,11 @@
 package com.mycompany.mda;
 
+import static java.lang.System.out;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,20 +13,28 @@ import java.sql.Statement;
  */
 public class dbHandler {
 
-    private static ResultSet getData(String sql) {
+    public static ArrayList<Buses> getData(String sql) {
         Connection c = null;
         Statement stmt = null;
         ResultSet result = null;
         String output = "";
+        ArrayList<Buses> busesList = new ArrayList<>();
 
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\hecku\\OneDrive\\Documentos\\tracking.db");
+            String dbLocation = "jdbc:sqlite:C:\\Users\\Heckutoru\\Documents\\NetBeansProjects\\MDA-master\\src\\main\\webapp\\mda.db";
+            c = DriverManager.getConnection(dbLocation);
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
             result = stmt.executeQuery(sql);
-
+            while (result.next()) {
+                Buses busStop = new Buses(result.getInt("line_number"));
+                busStop.setStopNumber(result.getInt("stop_number"));
+                busStop.setLatitude(result.getDouble("latitude"));
+                busStop.setLongitud(result.getDouble("longitud"));
+                busesList.add(busStop);
+            }
             stmt.close();
             c.commit();
             c.close();
@@ -32,6 +42,6 @@ public class dbHandler {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        return result;
+        return busesList;
     }
 }
